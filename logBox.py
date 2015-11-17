dc4385dc66ac66d88a523b0825de391d2920e0f5
JasonG-FR <jason.gombert@gmail.com>
2015-11-17 23:24:56 +0100

    Ajout et prise en charge d'un fichier de configuration

--- logBox.py
+++ logBox.py
@@ -8,24 +8,51 @@ import socket
 import fcntl
 import struct
 
-debug = True
-tps_actu = 5                                                                                             #temps à attendre entre deux vérifications
-cheminWebdav = "/home/jason/Desktop/WebDav_Test"          #Rpi /home/pi/Box/VPN_IP
-interface = "wlp3s0"                                                                                #Rpi tun0
-destinataireGPG = "opo"                                                                        #Rpi opo
+def genDefauts():
+    params = {"debug":False,
+                         "tps_actu":"300",
+                         "cheminWebdav":"/home/pi/Box/VPN_IP",
+                         "interface":"tun0",
+                         "destinataireGPG":"opo"}
+                         
+    config = open("config","w")
+    for key in params:
+        config.write(str(key)+":"+str(params[key])+"\n")
+    config.close()
+    
+def lireParametres():
+    try:
+        config = open("config","r")
+    except IOError:
+        genDefauts()
+        config = open("config","r")
+    params = formaterParams(config)
+    config.close()
+    return params
+    
+def formaterParams(fichier):
+    params = {}     #inutile?
+    for line in fichier:
+            key = line.split(":")[0]
+            item = line.split(":")[1]
+            if key == "debug":
+                params[key] = bool(item)
+            else:
+                params[key] = item.replace("\n","")
+    return params
 
 def lireIP(interface, debug):
+    if debug:
+        print interface
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     ip = socket.inet_ntoa(fcntl.ioctl(s.fileno(),0x8915,struct.pack('256s', interface[:15]))[20:24])
     if debug:
         print ip
     return ip
     
-def ecrireIP(IP,destinataireGPG,debug):
-    date_cmd = os.popen("date +%d%m%Y_%H%M%S")
-    #Retourne une chaine : 11112015_235551
+def ecrireIP(IP,destinataireGPG,cheminWebdav,debug):
+    date_cmd = os.popen("date +%d%m%Y_%H%M%S")          #Retourne une chaine : 11112015_235551
     date = date_cmd.read().replace("\n","")
-    
     nom = "ipVPNchi_" + date + ".txt"
     
     if debug:
@@ -60,19 +87,27 @@ def lireSauvegardeIP(debug):
         print ip
     return ip
 
-if debug:
+
+
+parametres = lireParametres()
+ip_ref  = lireSauvegardeIP(parametres["debug"])        #Permet de charger la dernière IP téléchargée sur le webdav
+
+if parametres["debug"]:
     print "Début pause..."
+
 os.system("sleep 60")   #On attend que tout soit initialisé avant de commencer (lan,openvpn,webdav)
-if debug:
+
+if parametres["debug"]:
     print "Fin pause..."
 
-ip_ref  = lireSauvegardeIP(debug)        #Permet de charger la dernière IP téléchargée sur le webdav
 while True:
-    ip_lue = lireIP(interface,debug)
-    if debug:
+    ip_lue = lireIP(parametres["interface"],parametres["debug"])        #On récupère l'IP actuelle
+    
+    if parametres["debug"]:
         print "ip_lue != ip_ref : " + str(ip_lue != ip_ref)
-    if ip_lue != ip_ref:
-        ecrireIP(ip_lue,destinataireGPG,debug)      #Mise à jour de l'adresse sur le webdav uniquement si l'IP a changée au reboot ou à cause du bail
-        sauvegarderIP(ip_lue)      #Mise à jour du fichier de sauvegarde local pour pouvoir connaître l'ip d'avant un reboot non planifié
-        ip_ref = ip_lue
-    os.system("sleep " + str(tps_actu))
+    
+    if ip_lue != ip_ref:                                                                                                                                                                            #Si celle qu'on a sur le webdav est différente alors on doit la mettre à jour !
+        ecrireIP(ip_lue,parametres["destinataireGPG"],parametres["cheminWebdav"],parametres["debug"])        #Mise à jour de l'adresse sur le webdav uniquement si l'IP a changée au reboot ou à cause du bail DHCP
+        sauvegarderIP(ip_lue)                                                                                                                                                              #Mise à jour du fichier de sauvegarde local pour pouvoir connaître l'ip d'avant un reboot non planifié
+        ip_ref = ip_lue                                                                                                                                                                              #On garde la nouvelle IP écrite en mémoire
+    os.system("sleep " + parametres["tps_actu"])                                                                                                                       #On attend avant de faire de nouveau une lecture de l'IP
